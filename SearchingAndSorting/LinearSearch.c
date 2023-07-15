ce9e3f102b2a2a0309a9bab6616c8649f1869329
Hritam Dey <113431333+dey-hritam@users.noreply.github.com>
2023-07-15 14:34:56 +0530

    Update LinearSearch.c

--- SearchingAndSorting/LinearSearch.c
+++ SearchingAndSorting/LinearSearch.c
@@ -1,3 +1,4 @@
+//Find the given element using Linear Search method.
 #include <stdio.h>
 int main()
 {
@@ -28,3 +29,15 @@ int main()
         printf("number is not found!");
     }
 }
+/* output::-
+    
+Enter a[0]:2
+
+Enter a[1]:7
+
+Enter a[2]:8
+
+Enter a[3]:5
+Enter the target element:5
+number is found
+*/
